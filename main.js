444775fcbe8e8583e71f8736c049153ec6cbc00c
arizinha2007 <147612815+arizinha2007@users.noreply.github.com>
2024-10-31 12:03:29 -0300

    .

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 function criarCartao(pergunta,resposta){
 let conteudo = document.getElementById("conteudo")
 let cartao = document.createElement("article")
+cartao.classList = "cartao"
+
+cartao.innerHTML = "testando o inner"
+
+conteudo.appendChild(cartao)
 }
+
 
 criarCartao(1,2)
