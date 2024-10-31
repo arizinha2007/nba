7db2218ed6ca917594f0a343c62c40bb222d6c4d
arizinha2007 <147612815+arizinha2007@users.noreply.github.com>
2024-10-31 12:10:30 -0300

    .

--- main.js
+++ main.js
@@ -3,9 +3,15 @@ let conteudo = document.getElementById("conteudo")
 let cartao = document.createElement("article")
 cartao.classList = "cartao"
 
-cartao.innerHTML = "testando o inner"
+cartao.innerHTML = `
+ <div class="cartao--conteudo">
+          <div class="cartao--pergunta">Qual é o time com mais titulos?</div>
+            <div class="cartao--resposta">Boston Celtics</div>
+        </div>
+        `
 
 conteudo.appendChild(cartao)
+
 }
 
 
